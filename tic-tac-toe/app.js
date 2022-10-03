85eca3c93a13d84655df2d5ac73b0967272e1927
marcosrglz <marcosrglez@gmail.com>
2022-10-03 12:52:26 +0200

    First Commit Tic Tac Toe JS: draw board with js, on click td changes

---
+++ tic-tac-toe/app.js
@@ -0,0 +1,52 @@
+let board = [
+  [
+    0, 0, 0
+  ],
+  [
+    0, 0, 0
+  ],
+  [
+    0, 0, 0
+  ]
+]
+
+const tbodyEl = document.querySelector("tbody")
+
+function drawBoard(){
+  let boardResult = [[]]
+  tbodyEl.innerHTML = ""
+
+  for(let i=0; i<board.length; i++) {
+    const trEl = document.createElement("tr")
+    tbodyEl.appendChild(trEl)
+    for(let j=0; j<board[i].length; j++) {
+      const tdEl = document.createElement("td")
+      trEl.appendChild(tdEl)
+      const imgEl = document.createElement("img")
+      if(board[i][j] == 0) {
+        tdEl.textContent = ""
+      } else if(board[i][j] == 1) {
+
+        imgEl.setAttribute("class", "game-circle")
+      } else if(board[i][j] == 2) {
+        imgEl.setAttribute("class", "game-x")
+      }
+      tdEl.appendChild(imgEl)
+    }
+  }
+}
+
+document.getElementById("board-table").addEventListener("click", (event) => {
+  if(event.target.tagName == "TD") {
+    const trEl = event.target.parentNode
+    const trIndex = Array.from(tbodyEl.children).indexOf(trEl)
+
+    const tdIndex = Array.from(trEl.children).indexOf(event.target)
+    board[trIndex][tdIndex] = 1
+    drawBoard()
+  }
+})
+
+//Start game
+drawBoard()
+//finishGame()
