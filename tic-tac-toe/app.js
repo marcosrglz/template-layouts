be89aa82106baee76a2e64e76f05efd57bbd2464
marcosrglz <marcosrglez@gmail.com>
2022-10-04 10:54:02 +0200

    Add horizontal win check

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -10,6 +10,40 @@ let board = [
   ]
 ]
 
+let winner = false
+const winCondition = [
+  [
+    [0, 0], [0, 1], [0, 2]
+  ],
+  [
+    [1,0], [1, 1], [1, 2]
+  ],
+  [
+    [2, 0], [2, 1], [2,2]
+  ]
+]
+
+function checkWinner() {
+  let result = false
+
+  winCondition.forEach((condition) => {
+    const first = condition[0]
+    const second = condition[1]
+    const third = condition[2]
+    const value1 = board[first[0]][first[1]]
+    const value2 = board[second[0]][second[1]]
+    const value3 = board[third[0]][third[1]]
+    
+    if(value1 != 0 || value2 != 0 || value3 != 0) {
+      if(value1 == value2 && value2 == value3 && value1 == value3){
+        result = value1
+      }
+    }
+  })
+
+  return result
+}
+
 const tbodyEl = document.querySelector("tbody")
 
 let currentPlayer = 2;
@@ -39,6 +73,10 @@ function drawBoard(){
 
 document.getElementById("board-table").addEventListener("click", (event) => {
   if(event.target.tagName == "TD") {
+    if(winner){
+      return
+    }
+
     const trEl = event.target.parentNode
     const trIndex = Array.from(tbodyEl.children).indexOf(trEl)
 
@@ -53,12 +91,17 @@ document.getElementById("board-table").addEventListener("click", (event) => {
     } else {
       currentPlayer = 1
     }
-    
+
     drawBoard()
     
+    //Win check
+    winner = checkWinner()
+    console.log(winner)
+    if(winner > 0){
+      alert('Winner')
+    }
   }
 })
 
 //Start game
 drawBoard()
-//finishGame()
