c33a48aa1e3b984a53273e07b8c1715d632510fc
marcosrglz <marcosrglez@gmail.com>
2022-10-27 10:10:19 +0200

    Added trending section and changes on responsive

---
+++ map-testing/app.js
@@ -0,0 +1,16 @@
+let map = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 16});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function onMapClick(e) {
+    const popup = L.popup();
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
